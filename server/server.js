be79e38988c63566640f08f7f8948716da0556cd
RoHobie <ronitthakur267@gmail.com>
2025-12-17 03:41:39 +0530

    add routes and endpoint

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import router from './routes/userRoutes.js';
 
 // App setup
 const app = express();
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use(cors());
 
 // api endpoints
+app.use('/api/user', router);
+
 app.get('/', (req, res) => {
   res.send('Welcome to the X-stop API');
 });
