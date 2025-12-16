d803dbcb5c3421aeb906b92c7454363695ee8061
RoHobie <ronitthakur267@gmail.com>
2025-12-17 03:40:16 +0530

    define login/signup and admin routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, getUserProfile } from '../controllers/userController.js';
+import { registerUser, loginUser, adminLogin } from '../controllers/userController.js';
 
 const router = express.Router();
 
